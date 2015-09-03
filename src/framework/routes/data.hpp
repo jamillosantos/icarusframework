e98fe1ca141744265acb9166c311a539febb7aba
J. Santos <jamillo@gmail.com>
2015-09-03 04:48:51 -0500

    Implements the 'routes' file parser
    This is the first implementation with a basic test.

---
+++ src/framework/routes/data.hpp
@@ -0,0 +1,207 @@
+/*
+ * @author Jamillo Santos <jamillo@gmail.com>
+ * @date August 18, 2015
+ */
+
+#ifndef ICARUSFRAMEWORK_ROUTES_DATA_HPP
+#define ICARUSFRAMEWORK_ROUTES_DATA_HPP
+
+#define INPUT_STREAM_BUFFER_SIZE 4096
+
+#include <string>
+#include <vector>
+#include <boost/filesystem.hpp>
+#include <stdexcept>
+#include <fstream>
+
+namespace icarus
+{
+namespace routes
+{
+class RegexToken
+{
+private:
+	std::string _name;
+	std::string _regex;
+public:
+	RegexToken(const std::string &name, const std::string &regex)
+		: _name(name), _regex(regex)
+	{ }
+
+	RegexToken(const std::string &regex)
+		: _name(""), _regex(regex)
+	{ }
+
+	const std::string &name() const
+	{
+		return this->_name;
+	}
+
+	const std::string &regex() const
+	{
+		return this->_regex;
+	}
+};
+
+class MethodParam
+{
+public:
+	enum MethodType { NORMAL, REFERENCE, POINTER };
+
+	MethodParam()
+	{ }
+
+	MethodParam(const std::string type, const MethodType attribute, const std::string name)
+		: _type(type), _attribute(attribute), _name(name)
+	{ }
+
+	const std::string &type() const
+	{
+		return this->_type;
+	}
+
+	MethodParam &type(const std::string &type)
+	{
+		this->_type = type;
+		return *this;
+	}
+
+	const MethodType &attribute() const
+	{
+		return this->_attribute;
+	}
+
+	MethodParam &attribute(const MethodType &attr)
+	{
+		this->_attribute = attr;
+		return *this;
+	}
+
+	const std::string &name() const
+	{
+		return this->_name;
+	}
+
+	MethodParam &name(const std::string &name)
+	{
+		this->_name = name;
+		return *this;
+	}
+
+private:
+	std::string _type;
+	MethodType _attribute;
+	std::string _name;
+};
+
+class CallMethod
+{
+private:
+	bool _static;
+
+	std::string _path;
+
+	std::vector<MethodParam> _params;
+public:
+	CallMethod()
+	{ }
+
+	CallMethod(CallMethod &callMethod)
+		: _path(callMethod._path), _params(callMethod._params)
+	{ }
+
+	CallMethod(const CallMethod &callMethod)
+		: _path(callMethod._path), _params(callMethod._params)
+	{ }
+
+	bool isStatic()
+	{
+		return this->_static;
+	}
+
+	CallMethod& setStatic(bool _static)
+	{
+		this->_static = _static;
+		return *this;
+	}
+
+	const std::string &path() const
+	{
+		return this->_path;
+	}
+
+	CallMethod& path(const std::string &path)
+	{
+		this->_path = path;
+		return *this;
+	}
+
+	const std::vector<MethodParam> &params() const
+	{
+		return this->_params;
+	}
+
+	CallMethod& addParam(std::string type, MethodParam::MethodType attribute, std::string name)
+	{
+		this->_params.emplace_back(type, attribute, name);
+		return *this;
+	}
+};
+
+class RoutesLine
+{
+private:
+	size_t line;
+
+	std::string _httpMethod;
+	std::vector<RegexToken> _regex;
+	CallMethod _callMethod;
+public:
+	RoutesLine(size_t line)
+		: line(line)
+	{ }
+
+	const std::string &httpMethod() const
+	{
+		return this->_httpMethod;
+	}
+
+	RoutesLine &httpMethod(const std::string &httpMethod)
+	{
+		this->_httpMethod = httpMethod;
+		return *this;
+	}
+
+	const std::vector<RegexToken> &regex() const
+	{
+		return this->_regex;
+	}
+
+	RoutesLine& addToken(std::string name, std::string regex)
+	{
+		this->_regex.emplace_back(name, regex);
+		return *this;
+	}
+
+	RoutesLine& addToken(std::string expression)
+	{
+		this->_regex.emplace_back(expression);
+		return *this;
+	}
+
+	const CallMethod &callMethod() const
+	{
+		return this->_callMethod;
+	}
+
+	RoutesLine& callMethod(CallMethod &callMethod)
+	{
+		this->_callMethod = callMethod;
+		return *this;
+	}
+};
+
+}
+}
+
+#endif // ICARUSFRAMEWORK_ROUTES_DATA_HPP
---
+++ src/framework/routes/parser.hpp
@@ -0,0 +1,436 @@
+/*
+ * @author Jamillo Santos <jamillo@gmail.com>
+ * @date August 18, 2015
+ */
+
+#ifndef ICARUSFRAMEWORK_ROUTES_COMPILER_HPP
+#define ICARUSFRAMEWORK_ROUTES_COMPILER_HPP
+
+#define INPUT_STREAM_BUFFER_SIZE 4096
+
+#include <string>
+#include <boost/filesystem.hpp>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
+#include "data.hpp"
+
+#include <boost/regex.hpp>
+
+namespace icarus
+{
+namespace routes
+{
+class Parser
+{
+private:
+	static boost::regex variablesName;
+
+	std::vector<RoutesLine> _lines;
+
+	std::unique_ptr<std::istream> istream;
+
+	std::string outputFolder;
+
+	char inputStreamBuffer[INPUT_STREAM_BUFFER_SIZE];
+	size_t inputStreamBufferSize;
+	size_t currentInputStreamChar;
+
+	size_t currentLine;
+	size_t currentChar;
+
+	char lastChar()
+	{
+		return this->inputStreamBuffer[this->currentInputStreamChar - 1];
+	}
+
+	bool readChar(char *cc)
+	{
+		if ((this->currentInputStreamChar >= INPUT_STREAM_BUFFER_SIZE) || (this->inputStreamBufferSize == 0))
+		{
+			this->inputStreamBufferSize = this->istream->readsome(this->inputStreamBuffer, INPUT_STREAM_BUFFER_SIZE);
+			this->currentInputStreamChar = 0;
+		}
+		else if (this->currentInputStreamChar >= this->inputStreamBufferSize)
+		{
+			this->inputStreamBuffer[this->currentInputStreamChar - 1] = '\0';
+			return false;
+		}
+
+		*cc = this->inputStreamBuffer[this->currentInputStreamChar++];
+		if (*cc == '\n')
+		{
+			this->currentLine++;
+			this->currentChar = 0;
+		}
+		else
+			this->currentChar++;
+
+		return true;
+	}
+
+	void readUntilNonBlank(char *cc)
+	{
+		while (this->readChar(cc))
+		{
+			if (!((*cc == '\n') || (*cc == '\r') || (*cc == ' ') || (*cc == '\t')))
+				return;
+		}
+		// TODO: Specialize exception
+		std::cerr << "Unexpected end of file." << std::endl;
+		throw new std::exception();
+	}
+
+	std::string readUntil(std::string chars)
+	{
+		char cc;
+		std::string result;
+		while (this->readChar(&cc))
+		{
+			if (result.find(cc) == std::string::npos)
+				result += cc;
+			else
+				return result;
+		}
+		// TODO: Specialize exception
+		std::cerr << "Unexpected end of file." << std::endl;
+		throw new std::exception();
+	}
+
+	void runCommentLine()
+	{
+		char cc;
+		while (this->readChar(&cc))
+		{
+			if (cc == '\n')
+				break;
+		}
+	}
+
+	void runCommentMultiLine()
+	{
+		char cc;
+		while (this->readChar(&cc))
+		{
+			if (cc == '*')
+			{
+				if (this->readChar(&cc))
+				{
+					if (cc == '/')
+					{
+						break;
+					}
+				}
+				else
+				{
+					// Throw an exception
+					std::cerr << "Unexpected end of file." << std::endl;
+					throw std::exception();
+				}
+			}
+		}
+	}
+
+	std::string runSkipQuotes()
+	{
+		char cc;
+		std::stringstream stream;
+		while (this->readChar(&cc))
+		{
+			stream << cc;
+			if (cc == '\\')
+			{
+				if (!this->readChar(&cc))
+				{
+					break;
+				}
+				stream << cc;
+			}
+			else if (cc == '"')
+			{
+				stream << cc;
+				return stream.str();
+			}
+		}
+		// TODO: Specialize exception
+		std::cerr << "Unexpected end of file." << std::endl;
+		throw std::exception();
+	}
+
+	std::string runSkipRegexBrackets()
+	{
+		unsigned int count = 1;
+		char cc;
+		std::stringstream stream;
+		while (this->readChar(&cc))
+		{
+			stream << cc;
+			if (cc == '\\')
+			{
+				if (!this->readChar(&cc))
+					break;
+				stream << cc;
+			}
+			else if (cc == '(')
+			{
+				count++;
+			}
+			else if (cc == ')')
+			{
+				count--;
+				if (count == 0)
+				{
+					return stream.str();
+				}
+			}
+		}
+		// TODO: Specialize exception
+		std::cerr << "Unexpected end of file." << std::endl;
+		throw std::exception();
+	}
+
+	void runLineMethodParameters(CallMethod &callMethod)
+	{
+		char cc;
+		std::stringstream stream, paramstream;
+		this->readUntilNonBlank(&cc);
+		for (;;)
+		{
+			if ((cc == ')') || (cc == ','))
+			{
+				if ((paramstream.tellp() == 0) && (stream.tellp() == 0))
+					break;
+				else
+				{
+					callMethod.addParam(paramstream.str(), MethodParam::MethodType::NORMAL, stream.str());
+					paramstream.str("");
+					stream.str("");
+					if (cc == ')')
+						break;
+					else
+						this->readUntilNonBlank(&cc);
+				}
+			}
+			else if ((cc == ' ') || (cc == '\t') || (cc == '\r') || (cc == '\n'))
+			{
+				if (paramstream.tellp())
+					paramstream << " ";
+				paramstream << stream.rdbuf();
+				stream.str("");
+				this->readUntilNonBlank(&cc);
+			}
+			else
+			{
+				stream << cc;
+				if (!this->readChar(&cc))
+				{
+					// TODO: Specialize exception
+					std::cerr << "Unexpected end of file." << std::endl;
+					throw std::exception();
+				}
+			}
+		}
+	}
+
+	void runLine()
+	{
+		this->_lines.emplace_back(this->currentLine);
+		RoutesLine &line = this->_lines.back();
+		std::stringstream stream;
+		char cc = this->lastChar();
+		stream << cc;
+		while (this->readChar(&cc))
+		{
+			if ((cc == ' ') || (cc == '\t'))
+			{
+				line.httpMethod(stream.str());
+				stream.str("");
+
+				this->readUntilNonBlank(&cc);
+				stream << cc;
+				while (this->readChar(&cc))
+				{
+					if (cc == '\\')
+					{
+						stream << '\\' << cc;
+					}
+					else if (cc == '<')
+					{
+						bool success = false;
+						std::string tmpstr = stream.str();
+						if (!tmpstr.empty())
+						{
+							line.addToken(tmpstr);
+							stream.str("");
+						}
+						while (this->readChar(&cc))
+						{
+							if (cc == ':')
+							{
+								tmpstr = stream.str();
+								stream.str("");
+
+								while (this->readChar(&cc))
+								{
+									if (cc == '>')
+									{
+										line.addToken(tmpstr, stream.str());
+										stream.str("");
+										success = true;
+										break;
+									}
+									else if (cc == '\\')
+									{
+										stream << cc;
+										if (!this->readChar(&cc))
+											break;
+										stream << cc;
+									}
+									else
+										stream << cc;
+								}
+							}
+							else if ((cc == ' ') || (cc == '\t') || (cc == '\r') || (cc == '\n'))
+								break;
+							else if (boost::regex_match(std::to_string(cc), variablesName))
+								stream << cc;
+							else
+							{
+								// TODO: specialize exception
+								std::cerr << "Invalid char." << std::endl;
+								throw std::exception();
+							}
+						}
+						if(!success)
+						{
+							// TODO: specialize exception
+							std::cerr << "Unexpected end of file." << std::endl;
+							throw std::exception();
+						}
+					}
+					else if ((cc == ' ') || (cc == '\t'))
+					{
+						std::string tmpstr = stream.str();
+						if (!tmpstr.empty())
+							line.addToken(tmpstr);
+						stream.str("");
+
+						this->readUntilNonBlank(&cc);
+						stream << cc;
+						CallMethod callMethod;
+						while (this->readChar(&cc))
+						{
+							if (cc == '(')
+							{
+								callMethod.path(stream.str());
+								stream.str("");
+								this->runLineMethodParameters(callMethod);
+								line.callMethod(callMethod);
+								return;
+							}
+							else if (cc == '\n')
+							{
+								// TODO: specialize exception
+								std::cerr << "Route declaration incomplete." << std::endl;
+								throw std::exception();
+							}
+							else
+							{
+								stream << cc;
+							}
+						}
+					}
+					else if (cc == '\n')
+					{
+						// TODO: specialize exception
+						std::cerr << "Route declaration incomplete." << std::endl;
+						throw std::exception();
+					}
+				}
+			}
+			else if (cc == '\n')
+			{
+				// TODO: Specialize exception
+				std::cerr << "Incomplete declaration." << std::endl;
+				throw std::exception();
+			}
+			else
+				stream << cc;
+		}
+		// TODO: Specialize exception
+		std::cerr << "Unexpected end of file." << std::endl;
+		throw std::exception();
+	}
+
+	void runDoc()
+	{
+		char cc;
+		while (this->readChar(&cc))
+		{
+			if (cc == '/')
+			{
+				if (this->readChar(&cc))
+				{
+					if (cc == '/')
+					{
+						this->runCommentLine();
+					}
+					else if (cc == '*')
+					{
+						this->runCommentMultiLine();
+					}
+				}
+				else
+				{
+					// Throw an exception
+					std::cerr << "Unexpected end of file." << std::endl;
+					throw std::exception();
+				}
+			}
+			else if ((cc != '\n') && (cc != '\r') && (cc != ' ') && (cc != '\t'))
+			{
+				this->runLine();
+			}
+		}
+	}
+public:
+	Parser()
+		: inputStreamBufferSize(0), currentInputStreamChar(0), currentLine(0), currentChar(0)
+	{ }
+
+	const std::vector<RoutesLine> &lines() const
+	{
+		return this->_lines;
+	}
+
+	void compile(std::string inputFile)
+	{
+		boost::filesystem::path ifp(inputFile);
+		if (boost::filesystem::exists(ifp))
+		{
+			this->istream.reset(new std::ifstream(ifp.string()));
+			if (this->istream)
+			{
+				this->runDoc();
+			}
+			else
+			{
+				// TODO: specialize exception
+				std::cerr << "Cannot open file." << std::endl;
+				throw std::exception();
+			}
+		}
+		else
+		{
+			// TODO: specialize exception
+			std::cerr << "Input file doesn't exists." << std::endl;
+			throw std::exception();
+		}
+	}
+};
+
+boost::regex Parser::variablesName("[a-zA-Z_0-9]+");
+}
+}
+
+#endif //ICARUSFRAMEWORK_HTTP_STATUSES_H
