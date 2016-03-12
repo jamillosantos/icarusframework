cd7581f436c33711d43f582e4cb2279a6fb4958a
J. Santos <jamillo@gmail.com>
2016-03-12 07:40:40 -0300

    General declarations

---
+++ include/icarus/general.h
@@ -0,0 +1,16 @@
+/**
+ * @author J. Santos <jamillo@gmail.com>
+ * @date March 09, 2016
+ **/
+
+#ifndef ICARUSFRAMEWORK_GENERAL_H
+#define ICARUSFRAMEWORK_GENERAL_H
+
+#include <string>
+
+namespace icarus
+{
+const std::string emptyString("");
+}
+
+#endif //ICARUSFRAMEWORK_GENERAL_H
---
+++ src/icarus/general.cpp
@@ -0,0 +1,6 @@
+/**
+ * @author J. Santos <jamillo@gmail.com>
+ * @date March 09, 2016
+ **/
+
+#include <icarus/general.h>
