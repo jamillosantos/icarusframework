9598db338e567a3c08b05a91c52a4a04ae3349ec
J. Santos <jamillo@gmail.com>
2016-03-14 04:33:20 -0300

    Updates cleaning script

--- build/debug/clean.sh
+++ build/debug/clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -R CMakeCache.txt CMakeFiles cmake_install.cmake CTestTestfile.cmake icarusframework libsimpleweb.a Makefile minotaur Testing tests libicarusframework.a libminotaur.a mntr icarus-fastcgi
+rm -R CMakeCache.txt CMakeFiles cmake_install.cmake CTestTestfile.cmake Makefile Testing tests libicarusframework.* libminotaur.* mntr icarus-fastcgi 
