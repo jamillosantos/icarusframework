ac571b4e0fda9abe05a353170b357fe1fc046b01
J. Santos <jamillo@gmail.com>
2016-04-09 09:16:59 -0300

    `icarus-routes` recompiles a file only when needed

--- src/icarus/routes/compiler.cpp
+++ src/icarus/routes/compiler.cpp
@@ -7,19 +7,26 @@
 
 void icarus::routes::compiler::compile(std::string from, std::string to)
 {
-	boost::filesystem::path fromPath(from);
-	parser parser(fromPath.parent_path().string());
-	document document(fromPath.stem().string());
-	parser.parse(from, document);
+	boost::filesystem::path from_path(from);
+	boost::filesystem::path to_path(to);
 
-	routes_writer writer;
-	std::stringstream memoryStream;
-	writer.write(memoryStream, document);
+	if (
+		(!boost::filesystem::exists(to_path))
+		|| (boost::filesystem::last_write_time(from_path) > boost::filesystem::last_write_time(to_path))
+	)
+	{
+		parser parser(from_path.parent_path().string());
+		document document(from_path.stem().string());
+		parser.parse(from, document);
 
-	boost::filesystem::path toPath(to);
-	boost::filesystem::create_directories(toPath.parent_path());
-	std::ofstream of(toPath.string());
-	of << memoryStream.rdbuf();
+		routes_writer writer;
+		std::stringstream memoryStream;
+		writer.write(memoryStream, document);
+
+		boost::filesystem::create_directories(to_path.parent_path());
+		std::ofstream of(to_path.string());
+		of << memoryStream.rdbuf();
+	}
 }
 
 void icarus::routes::compiler::compile(std::string from, std::ostream &to)
